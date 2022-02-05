bdf252a92ca780d095baf846e6b103f40555dc6d
Moonhee Hong <hmh94253862@gmail.com>
2022-02-05 23:31:10 +0900

    re

--- 1218List/src/main/java/kr/green/maven/List/SetEx2.java
+++ 1218List/src/main/java/kr/green/maven/List/SetEx2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package kr.green.maven.List;
 
 import java.util.Iterator;
@@ -27,33 +26,3 @@ public class SetEx2 {
 		for(int i : lotto) System.out.println(i);
 	}
 }
-=======
-package kr.green.maven.List;
-
-import java.util.Iterator;
-import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
-
-public class SetEx2 {
-	public static void main(String[] args) {
-		// Set<Integer> lotto = new HashSet<>();
-		Set<Integer> lotto = new TreeSet<>(); // TreeSet : 정렬이 지원되는 Set이다.
-		Random rnd = new Random();
-		while(lotto.size()<6) {
-			lotto.add(rnd.nextInt(45)+1);
-		}
-		
-		System.out.println("로또 번호 : " + lotto);
-		
-		// 반복 1
-		Iterator<Integer> it= lotto.iterator();
-		while(it.hasNext())
-			System.out.println(it.next());
-		System.out.println();
-		
-		// 반복 2
-		for(int i : lotto) System.out.println(i);
-	}
-}
->>>>>>> refs/remotes/origin/master
--- 1218List/src/main/java/kr/green/maven/List/SetEx4.java
+++ 1218List/src/main/java/kr/green/maven/List/SetEx4.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package kr.green.maven.List;
 
 import java.util.HashSet;
@@ -36,42 +35,3 @@ public class SetEx4 {
 		System.out.println( persons2.size() + "명 : " + persons2);
 	}
 }
-=======
-package kr.green.maven.List;
-
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-
-import kr.green.maven.vo.Person;
-
-//사용자가 만든 객체를 Set에 저장이 가능할까요?
-public class SetEx4 {
-	public static void main(String[] args) {
-		// 사용자가 만든 객체를 Set에 저장하려면 반드시 equals메서드를 오버라이딩 해야만 중복을 허용하지 않는다.
-		// equals메서드를 오버라이딩 하지 않으면 hashCode로 비교하여 중복을 허용하지 않는다. 
-		Set<Person> persons = new HashSet<>();
-		
-		persons.add(new Person("한사람", 32, true));
-		persons.add(new Person("두사람", 32, true));
-		persons.add(new Person("세사람", 22, false));
-		System.out.println( persons.size() + "명 : " + persons);
-		
-		persons.add(new Person("한사람", 32, true));
-		persons.add(new Person("두사람", 32, true));
-		persons.add(new Person("세사람", 22, false));
-		System.out.println( persons.size() + "명 : " + persons);
-		
-		// 사용자가 만든 객체를 TreeSet에 저장하려면 반드시 정렬을 지원하는 방법을 기술해 주어야 한다.
-		// 정렬을 지원하는 방법은 Comparable인터페이스를 구현해 주면 된다.
-		Set<Person> persons2 = new TreeSet<>();
-		persons2.add(new Person("한사람", 32, true));
-		persons2.add(new Person("한사람", 20, true));
-		persons2.add(new Person("두사람", 18, true));
-		persons2.add(new Person("두사람", 30, true));
-		persons2.add(new Person("세사람", 22, false));
-		persons2.add(new Person("세사람", 19, false));
-		System.out.println( persons2.size() + "명 : " + persons2);
-	}
-}
->>>>>>> refs/remotes/origin/master
