b3ae56b3e887e51ae62e331919265cce2e8cb952
Moonhee Hong <hmh94253862@gmail.com>
2022-02-13 23:17:14 +0900

    Gson, VO, ViewMovieList

---
+++ Java1208/src/main/java/kr/green/vo/MyVO.java
@@ -0,0 +1,13 @@
+package kr.green.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MyVO {
+	private String name;
+	private int age;
+}
